409a581ed1edbf6417b15e98709a236c9985b208
kanitaki <107522968+kanitaki@users.noreply.github.com>
2022-09-10 20:54:18 -0400

    Create PostInstallOne.sh
    First install script to setup the needed repositories and packages before the first reboot. This is specific to Fedora on an Asus Zephyrus G14(2020).

---
+++ PostInstallOne.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo;
+echo "Set fastest mirror for DNF";
+echo;
+echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf && echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf && echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf && sudo dnf update -yq && sudo dnf autoremove -yq ;
+echo;
+echo "Installing needed groups...";
+echo;
+sudo dnf group install "base-x" "Common NetworkManager Submodules" "Standard" "Printing Support" "Core" "Fonts" "Hardware Support" "System Tools" "C Development Tools and Libraries" "Development Tools" "Sound and Video" -yq;
+echo;
+echo "Installing flatpak";
+sudo dnf install flatpak -y;
+echo;
+echo "Run the second script after a reboot. Rebooting in 60 seconds."
+wait 60;
+reboot;
